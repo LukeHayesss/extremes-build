9da7b12f266c0bf7d85cba597f9ee1811f449c64
Luke Hayes <lukehayesa@gmail.com>
2022-11-08 13:24:19 -0500

    moreinfo btn

--- src/pages/MoreInfo.jsx
+++ src/pages/MoreInfo.jsx
@@ -8,7 +8,7 @@ const MoreInfo = () => {
             <Container>
                 <Header>More Info</Header>
                 <Icon>
-                    <FaArrowCircleDown size={100}/>
+                    <FaArrowCircleDown size={100} className="hvr-hang"/>
                 </Icon>
             </Container>
             
@@ -21,7 +21,6 @@ const Wrapper = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
-/* border-top: 1px solid #000000; */
 `
 
 const Header = styled.h1`
@@ -36,4 +35,69 @@ height: 20vh;
 const Icon = styled.div`
 color: #f4c430;
 cursor: pointer;
+&:hover {
+    @-webkit-keyframes hvr-hang {
+  0% {
+    -webkit-transform: translateY(8px);
+    transform: translateY(8px);
+  }
+  50% {
+    -webkit-transform: translateY(4px);
+    transform: translateY(4px);
+  }
+  100% {
+    -webkit-transform: translateY(8px);
+    transform: translateY(8px);
+  }
+}
+@keyframes hvr-hang {
+  0% {
+    -webkit-transform: translateY(8px);
+    transform: translateY(8px);
+  }
+  50% {
+    -webkit-transform: translateY(4px);
+    transform: translateY(4px);
+  }
+  100% {
+    -webkit-transform: translateY(8px);
+    transform: translateY(8px);
+  }
+}
+@-webkit-keyframes hvr-hang-sink {
+  100% {
+    -webkit-transform: translateY(8px);
+    transform: translateY(8px);
+  }
+}
+@keyframes hvr-hang-sink {
+  100% {
+    -webkit-transform: translateY(8px);
+    transform: translateY(8px);
+  }
+}
+.hvr-hang {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+}
+.hvr-hang:hover, .hvr-hang:focus, .hvr-hang:active {
+  -webkit-animation-name: hvr-hang-sink, hvr-hang;
+  animation-name: hvr-hang-sink, hvr-hang;
+  -webkit-animation-duration: .3s, 1.5s;
+  animation-duration: .3s, 1.5s;
+  -webkit-animation-delay: 0s, .3s;
+  animation-delay: 0s, .3s;
+  -webkit-animation-timing-function: ease-out, ease-in-out;
+  animation-timing-function: ease-out, ease-in-out;
+  -webkit-animation-iteration-count: 1, infinite;
+  animation-iteration-count: 1, infinite;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+  -webkit-animation-direction: normal, alternate;
+  animation-direction: normal, alternate;
+}
+}
 `
